8e7187ab00f8cdeeba9da65bcfa76f281a0e3b03
enochanok <hanoktamang03@gmail.com>
2024-04-02 14:42:27 +0545

    first commit

---
+++ back-end/src/main/java/com/carRentalService/gantavya/GantavyaApplication.java
@@ -0,0 +1,13 @@
+package com.carRentalService.gantavya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GantavyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GantavyaApplication.class, args);
+	}
+
+}
